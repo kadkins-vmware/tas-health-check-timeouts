62277c5438163ef6d90245e7587c85edb5dbaa23
kadkins@vmware.com <kadkins@vmware.com>
2021-07-30 13:14:07 -0500

    cleanup

--- src/main/java/com/vmware/tas/healthchecktimeouts/HealthCheckTimeouts.java
+++ src/main/java/com/vmware/tas/healthchecktimeouts/HealthCheckTimeouts.java
@@ -1,14 +1,10 @@
 package com.vmware.tas.healthchecktimeouts;
 
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @RestController
